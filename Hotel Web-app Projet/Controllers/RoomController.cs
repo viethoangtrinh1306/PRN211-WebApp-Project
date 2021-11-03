07eb282e6d9bac99a856ac63840ad8c00e3cc9f2
viethoang1306 <hoangvjpkuku@gmail.com>
2021-11-03 15:39:40 +0700

    Add project files.

---
+++ Hotel Web-app Projet/Controllers/RoomController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_Web_app_Projet.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RoomDetail()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hotel Web-app Projet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hotel_Web_app_Projet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
