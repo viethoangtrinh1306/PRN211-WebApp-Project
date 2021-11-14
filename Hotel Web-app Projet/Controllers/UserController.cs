a3fb0ad9c314425cbdb070c3e72bd42284f84d96
kien <kiennthe150935@fpt.edu.vn>
2021-11-14 17:50:38 +0700

    update user profile by kvjp

--- Hotel Web-app Projet/Controllers/UserController.cs
+++ Hotel Web-app Projet/Controllers/UserController.cs
@@ -18,9 +18,57 @@ namespace Hotel_Web_app_Projet.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult update(String name, String phone, bool gender)
+        {
+            
+            
+
+            Account account = JsonConvert.DeserializeObject<Account>(HttpContext.Session.GetString("account"));
+            User user = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("user"));
+            var data = context.Users.FirstOrDefault(x => x.UserId == user.UserId);
+            data.Name = name;
+            data.Phone = phone;
+            data.Gender = gender;
+
+
+            context.SaveChanges();
+            user = context.Users.Where(u => u.AccountId == account.AccountId).FirstOrDefault();
+
+            HttpContext.Session.SetString("user", JsonConvert.SerializeObject(user));
+            return RedirectToAction("UserDetails", "User");
+
+            
+        }
+
         public IActionResult ChangePassword()
         {
+
             return View();
+        }
+
+        [HttpPost] 
+        public IActionResult changePass(string pass, string newPass, string reNewPass)
+        {
+            Account account = JsonConvert.DeserializeObject<Account>(HttpContext.Session.GetString("account"));
+            User user = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("user"));
+            var data = context.Accounts.FirstOrDefault(x => x.AccountId == user.AccountId);
+
+            if(data.Password != pass)
+            {
+                ViewBag.error = "Wrong password!";
+                
+                return RedirectToAction("ChangePassword", "User");
+            } 
+            if(newPass != reNewPass)
+            {
+                ViewBag.error = "Check repass again!";
+                
+                return RedirectToAction("ChangePassword", "User");
+            }
+
+            
+            return RedirectToAction("ChangePassword", "User");
         }
     }
 }
