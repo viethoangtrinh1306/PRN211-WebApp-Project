cdc69f316752594d995f8d788c1056a33aaf1a98
viethoang1306 <hoangvjpkuku@gmail.com>
2021-11-14 22:29:12 +0700

    update  admin filter

--- Hotel Web-app Projet/Controllers/AdminController.cs
+++ Hotel Web-app Projet/Controllers/AdminController.cs
@@ -1,34 +1,76 @@
 ﻿using Hotel_Web_app_Projet.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace Hotel_Web_app_Projet.Controllers
 {
     public class AdminController : Controller
     {
+      
         HotelWebsiteContext context = new();
         public override ViewResult View()
         {
+
             ViewBag.RoomTypes = context.RoomTypes.ToList();
             return base.View();
         }
+
+        public bool IsAdmin()
+        {
+            Account account = new Account();
+            if (HttpContext.Session.GetString("account") != null)
+            {
+                account = JsonConvert.DeserializeObject<Account>(HttpContext.Session.GetString("account"));
+            }
+            if (account.AuthorId != 2)
+            {
+                return false;
+            }
+            else { return true; }
+        }
         public IActionResult UserManagement()
         {
-            ViewBag.UserList = context.Users.ToList();
-            return View();
+            if (IsAdmin())
+            {
+                ViewBag.UserList = context.Users.ToList();
+                return View();
+            }
+            else return RedirectToAction("Error404", "admin");
         }
         public IActionResult RoomManagement()
         {
-            ViewBag.RoomList = context.Rooms.ToList();
+            if (IsAdmin())
+            {
+                ViewBag.RoomList = context.Rooms.ToList();
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Error404", "admin");
+            }
+            
+        }
+        public IActionResult Error404()
+        {
             return View();
         }
         public IActionResult BookingManagement()
         {
-            ViewBag.BookingList = context.Bookings.ToList();
-            return View();
+            if (IsAdmin())
+            {
+                ViewBag.BookingList = context.Bookings.ToList();
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Error404", "admin");
+            }
+           
         }
     }
 }
