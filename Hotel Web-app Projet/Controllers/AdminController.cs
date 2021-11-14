9a172371ab174c2917a0c620192ad32db9bd543c
Le Thanh Trung <lethanhtrung2719@gmail.com>
2021-11-14 23:51:34 +0700

    add users management

--- Hotel Web-app Projet/Controllers/AdminController.cs
+++ Hotel Web-app Projet/Controllers/AdminController.cs
@@ -35,24 +35,31 @@ namespace Hotel_Web_app_Projet.Controllers
             }
             else { return true; }
         }
-        public IActionResult UserManagement()
+        public IActionResult UserManagement(int page)
         {
-            //Pagination
-            int pageSize = 10;
-            decimal pageNumber = Math.Ceiling((decimal)context.Users.ToList().Count / pageSize);
+            if (IsAdmin())
+            {
+                //Pagination
+                int pageSize = 10;
+                decimal pageNumber = Math.Ceiling((decimal)context.Users.ToList().Count / pageSize);
 
-            if (page <= 1)
-            {
-                page = 1;
+                if (page <= 1)
+                {
+                    page = 1;
+                }
+                else if (page >= pageNumber)
+                {
+                    page = (int)pageNumber;
+                }
+                ViewBag.UserList = context.Users.ToPagedList(page, pageSize);
+                ViewBag.Pages = pageNumber;
+                ViewBag.CurrentPage = page;
+                return View();
             }
-            else if (page >= pageNumber)
+            else
             {
-                page = (int)pageNumber;
+                return RedirectToAction("Error404", "Admin");
             }
-            ViewBag.UserList = context.Users.ToPagedList(page, pageSize);
-            ViewBag.Pages = pageNumber;
-            ViewBag.CurrentPage = page;
-            return View();
         }
         [HttpPost]
         public IActionResult ChangeAccountStatus()
@@ -78,42 +85,115 @@ namespace Hotel_Web_app_Projet.Controllers
             return RedirectToAction("UserManagement","Admin");
         }
 
+        [HttpGet]
         public IActionResult RoomManagement(int page)
         {
-            //Pagination
-            int pageSize = 10;
-            decimal pageNumber = Math.Ceiling((decimal)context.Rooms.ToList().Count / pageSize);
+            if (IsAdmin())
+            {
+                //Pagination
+                int pageSize = 10;
+                decimal pageNumber = Math.Ceiling((decimal)context.Rooms.ToList().Count / pageSize);
 
-            if (page <= 1)
-            {
-                page = 1;
+                if (page <= 1)
+                {
+                    page = 1;
+                }
+                else if (page >= pageNumber)
+                {
+                    page = (int)pageNumber;
+                }
+                ViewBag.RoomList = context.Rooms.ToPagedList(page, pageSize);
+                ViewBag.Pages = pageNumber;
+                ViewBag.CurrentPage = page;
+                ViewBag.RoomTypes = context.RoomTypes.ToList();
+                return View();
             }
-            else if (page >= pageNumber)
+            else
             {
-                page = (int)pageNumber;
+                return RedirectToAction("Error404", "Admin");
             }
-            ViewBag.RoomList = context.Rooms.ToPagedList(page, pageSize);
-            ViewBag.Pages = pageNumber;
-            ViewBag.CurrentPage = page;
-            return View();
         }
+
+        [HttpPost]
+        public IActionResult RoomManagement(string action)
+        {
+            if (IsAdmin())
+            {
+                if (action == "add")
+                {
+                    string name = Request.Form["name"];
+                    Room room = new Room
+                    {
+                        Name = Request.Form["name"],
+                        Position = Request.Form["position"],
+                        TypeId = int.Parse(Request.Form["roomType"]),
+                        Image = Request.Form["image"]
+                    };
+                    context.Rooms.Add(room);
+                    context.SaveChanges();
+                    TempData["message"] = "Add successfully!";
+                }
+                else if(action == "edit")
+                {
+                    int roomId = int.Parse(Request.Form["roomId"]);
+                    Room room = context.Rooms.SingleOrDefault(c => c.RoomId == roomId);
+                    if (room != null)
+                    {
+                        room.Name = Request.Form["name"];
+                        room.Position = Request.Form["position"];
+                        room.TypeId = int.Parse(Request.Form["roomType"]);
+                        room.Image = Request.Form["image"];
+                        context.SaveChanges();
+                        TempData["message"] = "Edit successfully!";
+                    }
+                }
+                else if(action == "delete")
+                {
+                    int roomId = int.Parse(Request.Form["roomId"]);
+                    Room room = context.Rooms.SingleOrDefault(c => c.RoomId == roomId);
+                    if (room != null)
+                    {
+                        context.Rooms.Remove(room);
+                        context.SaveChanges();
+                        TempData["message"] = "Delete successfully!";
+                    }
+                }
+                return RedirectToAction("RoomManagement", "Admin");
+            }
+            else
+            {
+                return RedirectToAction("Error404", "Admin");
+            }
+        }
+
         public IActionResult BookingManagement(int page)
         {
-            //Pagination
-            int pageSize = 10;
-            decimal pageNumber = Math.Ceiling((decimal)context.Bookings.ToList().Count / pageSize);
+            if (IsAdmin())
+            {
+                //Pagination
+                int pageSize = 10;
+                decimal pageNumber = Math.Ceiling((decimal)context.Bookings.ToList().Count / pageSize);
 
-            if (page <= 1)
-            {
-                page = 1;
+                if (page <= 1)
+                {
+                    page = 1;
+                }
+                else if (page >= pageNumber)
+                {
+                    page = (int)pageNumber;
+                }
+                ViewBag.BookingList = context.Bookings.ToPagedList(page, pageSize);
+                ViewBag.Pages = pageNumber;
+                ViewBag.CurrentPage = page;
+                return View();
             }
-            else if (page >= pageNumber)
+            else
             {
-                page = (int)pageNumber;
+                return RedirectToAction("Error404", "Admin");
             }
-            ViewBag.BookingList = context.Bookings.ToPagedList(page, pageSize);
-            ViewBag.Pages = pageNumber;
-            ViewBag.CurrentPage = page;
+        }
+        public IActionResult Error404()
+        {
             return View();
         }
     }
